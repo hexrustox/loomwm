2d2c7863098296929c163126bda4ef6b793f45c9
Codnixus <27430075-codnixus@users.noreply.gitlab.com>
2025-10-27 19:04:34 +0000

    Clean up nix shell

--- nix/override.nix
+++ nix/override.nix
@@ -1,29 +1,25 @@
-pkgs: {
+pkgs:
+let
+  lib = pkgs.lib;
+  mesa = pkgs.mesa;
+  mesa-drivers = [ mesa ];
+  vadrivers = [ ];
+  libvdpau = [ pkgs.libvdpau-va-gl ];
+  ld = with pkgs; [
+    libglvnd
+    wayland
+  ];
+in
+{
   buildInputs = with pkgs; [
     pkg-config
     libxkbcommon
   ];
-  shellHook =
-    let
-      lib = pkgs.lib;
-      mesa = pkgs.mesa;
-      mesa-drivers = [ mesa ];
-      vadrivers = [ ];
-      libvdpau = [ pkgs.libvdpau-va-gl ];
-      ld = [
-        pkgs.libglvnd
-        pkgs.wayland
-      ];
-    in
-    ''
-      export GBM_BACKENDS_PATH=${lib.makeSearchPathOutput "lib" "lib/gbm" mesa-drivers}
-      export LIBGL_DRIVERS_PATH=${lib.makeSearchPathOutput "lib" "lib/dri" mesa-drivers}
-      export LIBVA_DRIVERS_PATH=${lib.makeSearchPathOutput "out" "lib/dri" (mesa-drivers ++ vadrivers)}
-      ${
-        ''export __EGL_VENDOR_LIBRARY_FILENAMES=${mesa.drivers}/share/glvnd/egl_vendor.d/50_mesa.json"''${__EGL_VENDOR_LIBRARY_FILENAMES:+:$__EGL_VENDOR_LIBRARY_FILENAMES}"''
-      }
-      export LD_LIBRARY_PATH=${lib.makeLibraryPath mesa-drivers}:${
-        lib.makeSearchPathOutput "lib" "lib/vdpau" libvdpau
-      }:${lib.makeLibraryPath ld}"''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-    '';
+  GBM_BACKENDS_PATH = "${lib.makeSearchPathOutput "lib" "lib/gbm" mesa-drivers}";
+  LIBGL_DRIVERS_PATH = "${lib.makeSearchPathOutput "lib" "lib/dri" mesa-drivers}";
+  LIBVA_DRIVERS_PATH = "${lib.makeSearchPathOutput "out" "lib/dri" (mesa-drivers ++ vadrivers)}";
+  __EGL_VENDOR_LIBRARY_FILENAMES = "${mesa}/share/glvnd/egl_vendor.d/50_mesa.json";
+  LD_LIBRARY_PATH = "${lib.makeLibraryPath (mesa-drivers ++ ld)}:${
+    lib.makeSearchPathOutput "lib" "lib/vdpau" libvdpau
+  }";
 }
