4ade17ec5a236fe3395c7013927036318fbb25e1
Codnixus <27430075-codnixus@users.noreply.gitlab.com>
2026-03-21 19:06:19 +0000

    Add assistant model test case & refactor

--- assistant/src/lib.rs
+++ assistant/src/lib.rs
@@ -13,6 +13,8 @@ pub use train::train;
 
 #[cfg(test)]
 mod tests {
+    use std::path::PathBuf;
+
     use burn::backend::{Autodiff, NdArray, Wgpu};
     use rand::seq::SliceRandom;
     use test_case::test_case;
@@ -20,6 +22,25 @@ mod tests {
     use super::*;
 
     const ARTIFACT_DIR: &str = "/tmp/guide";
+
+    fn run_training(device: BackendDevice, artifact_dir: PathBuf, dataset: RankingDataset) {
+        match device {
+            BackendDevice::Gpu(d) => train::<Autodiff<Wgpu>>(artifact_dir, dataset, d),
+            BackendDevice::Cpu(d) => train::<Autodiff<NdArray>>(artifact_dir, dataset, d),
+        }
+    }
+
+    fn run_inference(
+        artifact_dir: PathBuf,
+        item: RankingItem,
+        device: BackendDevice,
+    ) -> Vec<String> {
+        match device {
+            BackendDevice::Gpu(d) => infer::<Wgpu>(artifact_dir, item, d),
+            BackendDevice::Cpu(d) => infer::<NdArray>(artifact_dir, item, d),
+        }
+        .unwrap()
+    }
 
     #[test_case(3, vec![RankingItem {
         app_ids: (1..=3).map(|n| n.to_string()).collect(),
@@ -49,21 +70,13 @@ mod tests {
         }
         items
     }; "random")]
-    fn test_model_identical_dataset(max: usize, items: Vec<RankingItem>) {
+    fn test_model_sequence_dataset(max: usize, items: Vec<RankingItem>) {
         let device = get_device();
 
         println!("{items:#?}");
 
         let dataset = RankingDataset::new(items);
-
-        match device.clone() {
-            BackendDevice::Gpu(d) => {
-                train::<Autodiff<Wgpu>>(ARTIFACT_DIR.into(), dataset, d);
-            }
-            BackendDevice::Cpu(d) => {
-                train::<Autodiff<NdArray>>(ARTIFACT_DIR.into(), dataset, d);
-            }
-        }
+        run_training(device.clone(), ARTIFACT_DIR.into(), dataset);
 
         let mut app_ids: Vec<_> = (1..=max).collect();
         while app_ids.is_sorted() {
@@ -73,16 +86,56 @@ mod tests {
         let app_ids = app_ids.iter().map(|n| n.to_string()).collect();
 
         let item = RankingItem { app_ids };
-        let result = match device {
-            BackendDevice::Gpu(d) => infer::<Wgpu>(ARTIFACT_DIR.into(), item, d),
-            BackendDevice::Cpu(d) => infer::<NdArray>(ARTIFACT_DIR.into(), item, d),
-        }
-        .unwrap()
-        .into_iter()
-        .map(|s| s.parse::<usize>().unwrap())
-        .collect::<Vec<_>>();
+        let result = run_inference(ARTIFACT_DIR.into(), item, device)
+            .into_iter()
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect::<Vec<_>>();
 
         println!("{result:?}");
         assert!(result.is_sorted());
+    }
+
+    #[test]
+    fn test_model_similar_app_id() {
+        let device = get_device();
+
+        let dataset = RankingDataset::new(vec![
+            RankingItem {
+                app_ids: vec![
+                    "editor A".to_string(),
+                    "terminal".to_string(),
+                    "browser".to_string(),
+                ],
+            },
+            RankingItem {
+                app_ids: vec![
+                    "editor B".to_string(),
+                    "terminal".to_string(),
+                    "browser".to_string(),
+                ],
+            },
+        ]);
+
+        run_training(device.clone(), ARTIFACT_DIR.into(), dataset);
+
+        let app_ids = vec![
+            "editor C".to_string(),
+            "terminal".to_string(),
+            "browser".to_string(),
+        ];
+        let mut app_ids_clone = app_ids.clone();
+        while app_ids_clone == app_ids {
+            app_ids_clone.shuffle(&mut rand::rng());
+        }
+        println!("{app_ids_clone:?}");
+
+        let item = RankingItem {
+            app_ids: app_ids_clone,
+        };
+
+        let result = run_inference(ARTIFACT_DIR.into(), item, device);
+
+        println!("{result:?}");
+        assert_eq!(result, app_ids)
     }
 }
